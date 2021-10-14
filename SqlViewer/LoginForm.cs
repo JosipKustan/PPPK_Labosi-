e412e3b1edf05e1ba0d2f573a990cec494e0f410
Josip Kustan <jkustan@croz.net>
2021-10-14 10:59:28 +0200

    init commit

---
+++ SqlViewer/LoginForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using Zadatak.Dal;
+
+namespace Zadatak
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+            FillDevelopmentLoginData();
+        }
+
+        private void FillDevelopmentLoginData()
+        {
+            //spajam se na Lukinu bazu :)
+            TbServer.Text = "sql.lcabraja.dev";
+            TbUserName.Text = "SA";
+            TbPassword.Text = "Pa$$word";
+        }
+
+        private void BtnLogIn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // exception driven, but ok
+                RepositoryFactory.GetRepository().LogIn(TbServer.Text.Trim(), TbUserName.Text.Trim(), TbPassword.Text.Trim());
+                new MainForm().Show();
+                Hide(); // we cannot dispose or close cause the child dies!
+                // do not forget to catch Form_Closing of child to kill the application!
+            }
+            catch (Exception ex)
+            {
+                LbError.Text = ex.Message;
+            }
+        }
+    }
+}
